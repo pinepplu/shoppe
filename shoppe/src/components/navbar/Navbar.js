0ea13ea33edea67927010f446bf6cb1a02ba2447
Meggie <meggiecheng01@gmail.com>
2023-02-13 00:43:22 -0500

    navbar

---
+++ shoppe/src/components/navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css'
+//import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <nav className='navbar'>
+        <div className='navbar-container'>
+          <b>this is the nabvar</b>
+          <br></br>
+          Home Womens Mens Search Cart 
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
