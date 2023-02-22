587eb6cdb39b2e87bce4e5d1fd49eea55a6683af
Meggie <meggiecheng01@gmail.com>
2023-02-22 02:42:29 -0500

    fixed footer and navbar

--- shoppe/src/components/footer/Footer.js
+++ shoppe/src/components/footer/Footer.js
@@ -4,34 +4,36 @@ import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
-    <div className='footer-container'>
-      <Link to='/' className='logo'><h4>Shoppe</h4></Link>
-      <div className='newsletter'>
-        <h4>Newsletter</h4>
-        <form>
-          <input type="text" placeholder='Email...' />
-          <button type='submit'>Subscribe</button>
-        </form>
-      </div>
-      <div className='socials'>
-        <h4>Socials</h4>
-        <p className='social-contents'>
-          <Link to='/'>Instagram</Link>
-          <br></br>
-          <Link to='/'>Facebook</Link>
-          <br></br>
-          <Link to='/'>GitHub</Link>
-        </p>
-      </div>
-      <div className='contact-us'>
-        <h4>Contact Us</h4>
-        <p className='contact-contents'>
-          Hours of Operation:
-          <br></br>
-          Monday - Friday 10:00AM to 5PM
-          <br></br>
-          Saturday - Sunday CLOSED
-        </p>
+    <div className='footer'>
+      <div className='footer-container'>
+      <Link to='/' className='footer-logo'><h4>Shoppé</h4></Link>
+        <div className='newsletter'>
+          <h4>Newsletter</h4>
+          <form>
+            <input type="text" placeholder='Email...' />
+            <button type='submit'>Subscribe</button>
+          </form>
+        </div>
+        <div className='socials'>
+          <h4>Socials</h4>
+          <p className='social-contents'>
+            <Link to='/'>Instagram</Link>
+            <br></br>
+            <Link to='/'>Facebook</Link>
+            <br></br>
+            <Link to='/'>GitHub</Link>
+          </p>
+        </div>
+        <div className='contact-us'>
+          <h4>Contact Us</h4>
+          <p className='contact-contents'>
+            Hours of Operation:
+            <br></br>
+            Monday - Friday 10:00AM to 5PM
+            <br></br>
+            Saturday - Sunday CLOSED
+          </p>
+        </div>
       </div>
       <div className='copyright'>
         &copy; Copyright
--- shoppe/src/components/navbar/Navbar.js
+++ shoppe/src/components/navbar/Navbar.js
@@ -5,13 +5,14 @@ import { Link } from 'react-router-dom'
 function Navbar() {
   return (
     <div className='navbar-container'>
-      <Link to='/' className='logo'>Shoppe</Link>
+      <Link to='/' className='nav-logo'>Shoppé</Link>
       <div className='search-bar'>
-        <input type="text" placeholder="Search..." />
+        <input type="text" className='search' placeholder="Search..." />
       </div>
       <ul>
         <li><Link to='/womens'>Womens</Link></li>
         <li><Link to='/mens'>Mens</Link></li>
+        <li><Link to='/login'>login</Link></li>
         <li><Link to='/cart'>Cart</Link></li>
       </ul>
     </div>
