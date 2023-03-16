b9eb623c8bdc4988f5f5dcc933306a6ebc466dbd
Meggie <meggiecheng01@gmail.com>
2023-03-16 00:57:46 -0400

    cart tut

--- shoppe/src/pages/Cart.js
+++ shoppe/src/pages/Cart.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ShoopingCart from '../components/products/ShoopingCart'
 
 function Cart() {
+  const [inCart, setInCart] = useState([])
+  const addToCart = (product) => {
+    const newItem = {
+      ...product,
+      count: 1,
+    }
+    setInCart([...inCart, newItem])
+  }
   return (
-    <>
-    <div>Shooping Cart</div>
-    </>
+    <div>
+      <ShoopingCart 
+      products={inCart}
+      />
+    </div>
   )
 }
 
--- shoppe/src/pages/Home.js
+++ shoppe/src/pages/Home.js
@@ -1,17 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { PRODUCTS } from '../components/products/Products'
-import Product from '../components/products/ProductDisplay'
+// import Product from '../components/products/ProductDisplay'
 import '../styles/Home.css'
+import { Plus } from 'phosphor-react'
+// import ShoopingCart from '../components/products/ShoopingCart'
+import Cart from './Cart'
 
 function Home() {
+  const [inCart, setInCart] = useState([])
+  const addToCart = (product) => {
+    const newItem = {
+      ...product,
+      count: 1,
+    }
+    setInCart([...inCart, newItem])
+  }
   return (
     <>
       <div className='home-title'><h1>Products</h1></div>
-      <section className='products'>
+      {/* <section className='products'>
         {PRODUCTS.map((product) => (
           <Product data={product} />
         ))}
-      </section>
+      </section> */}
+      {/* <ShoopingCart 
+        products={inCart}
+      /> */}
+      <Cart />
+      <section className='products'>
+          {PRODUCTS.map((product) => (
+            <div className='product-items' key={PRODUCTS.id}>
+              <div className='item-img'><img src={product.pImage} alt={product.pName} style={{ width: 200, height: 200 }} /></div>
+              <div className='item-name'><h4>{product.pName}</h4></div>
+              <div className='item-price'><p>Price: ${product.price}</p></div>
+              <button className='add-cart-btn' onClick={() => addToCart(product)}>Add To Cart <Plus size={20} /></button>
+            </div>
+          ))}
+        </section>
+
       <div className='container'>
         <div className='card'></div>
         <div className='card'></div>
